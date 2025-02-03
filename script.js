e00d7b7c1b342110bded5c4e581f483074b0c571
Gebhardt Lilla <gebhardt.lilla@ckik.loc>
2025-02-03 10:34:19 +0100

    elorehaladas 2.0

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const n= 5;
 const m = 12;
 const fox = { x: n-1, y: m-1 }; // x: sor, y: oszlop
 const nyuszi = {x: randint(0,n), y: randint(0,m/2)};
+let jatek_fut = false;
+let szint = 1;
+let ido = 0;
 
 const table = document.querySelector("table")
 
@@ -9,4 +12,6 @@ function randint(a,b){
     return Math.floor(Math.random() * (b-a+1)) + a;
 }
 
+function nyuszimuszi{
 
+}
